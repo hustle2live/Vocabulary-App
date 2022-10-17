bd373b10df424089f37d89384d4ead2a85d03e7b
hustle2live <87603877+hustle2live@users.noreply.github.com>
2022-10-17 22:28:13 +0300

    browser router changed to hash router

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { store } from './redux/redux-store';
 import { Provider } from 'react-redux';
 
@@ -9,10 +9,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
