c97681be1f7833ab15f8091ce9e3bd4fc0972c23
hustle2live <87603877+hustle2live@users.noreply.github.com>
2022-10-17 18:08:12 +0300

    clear comments

--- app/src/components/dictionary.js
+++ app/src/components/dictionary.js
@@ -6,8 +6,8 @@ export const dictionary = [
   { name: 'biochemistry', translate: 'біохемія' },
   { name: 'geodesy', translate: 'геодезія' },
   { name: 'verb', translate: 'дієслово' },
-  { name: 'putin', translate: 'хуйло' },
-  { name: 'bandera', translate: 'зсу' },
+  { name: 'putin', translate: 'х@йло' },
+  { name: 'bandera', translate: 'боєць зсу' },
   { name: 'russian soldier', translate: 'свинособака' },
   { name: 'lukashenko', translate: 'путана' },
   { name: 'slava Ukraini', translate: 'героям слава' }
