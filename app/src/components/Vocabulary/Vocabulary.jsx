c2e13a8be7059aff7bb5e0654bf64fd7733dae1e
hustle2live <87603877+hustle2live@users.noreply.github.com>
2022-10-30 22:07:19 +0200

    create vocabulary-module-scss, migrate styles and component

---
+++ app/src/components/Vocabulary/Vocabulary.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import styles from './Vocabulary.module.scss';
+
+export const Vocabulary = () => {
+  const store = useSelector((state) => state);
+  const vocabulary = store.vocabulary;
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const startNewTest = () => {
+    dispatch({
+      type: 'startNewTest'
+    });
+    dispatch({
+      type: 'changeToNextTest'
+    });
+    navigate('/test-page' + location.search);
+  };
+
+  const addNewWord = () => navigate('/add-new-word' + location.search);
+
+  return (
+    <div className={styles.wrapper}>
+      <header className={styles.header}>
+        <button>MY DICTIONARY</button> |{' '}
+        <button onClick={startNewTest}>START TESTING</button>|{' '}
+        <button onClick={addNewWord}>ADD NEW WORD</button>
+      </header>
+      <div className={styles.content}>
+        <ul className={styles.wordList}>
+          {vocabulary.map(({ name, translate }, index) => (
+            <li className={styles.wordListElement} key={index}>
+              <div className={styles['word-description']}>
+                <span></span>
+              </div>
+              <p className={styles['word-name']}>{name}</p>
+              <p className={styles['word-translate']}>{translate}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
