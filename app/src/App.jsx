cbf750457943ed9ca99d2ad421a3b805306debf6
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2022-10-30 16:51:37 +0200

    Migrate AddWord component to the CSS modules

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import { Vocabulary } from './components/Vocabulary';
-import { NewWordAddition } from './components/NewWordAddition';
+import { AddWord } from './components/AddWord/AddWord';
 import { TestInteractive } from './components/TestInteractive';
 
 import './styles/style.scss';
 
 const App = () => (
   <Routes>
-    <Route path='/' element={<Vocabulary />} />
-    <Route path='/add-new-word' element={<NewWordAddition />} />
-    <Route path='/test-page' element={<TestInteractive />} />
+    <Route path="/" element={<Vocabulary />} />
+    <Route path="/add-new-word" element={<AddWord />} />
+    <Route path="/test-page" element={<TestInteractive />} />
   </Routes>
 );
 
