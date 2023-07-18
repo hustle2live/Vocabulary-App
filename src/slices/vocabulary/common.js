c7fca84ec660dcd6da6766396f6fae55155cfd1f
hustle2live <kuz.v@ukr.net>
2023-07-18 23:28:18 +0300

    add action types and common.js

---
+++ src/slices/vocabulary/common.js
@@ -0,0 +1,12 @@
+const ActionTypes = {
+   ADD_WORD: 'vocabulary/add-word',
+   UPDATE_WORD: 'vocabulary/update-word',
+   DELETE_WORD: 'vocabulary/delete-word',
+   SET_ACHIEVED_WORD: 'vocabulary/set-achieved-word',
+   SET_PRACTICE_WORD: 'vocabulary/set-practice-word',
+   SORT_BY_NAME: 'vocabulary/sort-by-name',
+   SORT_BY_STATUS: 'vocabulary/sort-by-status',
+   SORT_RANDOM: 'vocabulary/sort-randomd'
+};
+
+export { ActionTypes };
