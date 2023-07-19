5b706ece06ecc5fdf719927861f00c0908315539
hustle2live <kuz.v@ukr.net>
2023-07-19 11:01:28 +0300

    moderate and restructure with builder & add.case the vocabulary.slice.js

--- src/slices/vocabulary/vocabulary.slice.js
+++ src/slices/vocabulary/vocabulary.slice.js
@@ -13,38 +13,44 @@ const vocabularyReducer = createSlice({
    initialState: defaultState,
    name: 'vocabulary',
    reducers: {},
-   extraКeducers: {
-      addWord: (state, action) => {
+   extraКeducers: (builder) => {
+      builder.addCase(addWord.fulfilled, (state, action) => {
          state.vocabulary = [...state.vocabulary, action.payload];
-      },
-      deleteWord: (state, action) => {
+      });
+
+      builder.addCase(deleteWord.fulfilled, (state, action) => {
          state.vocabulary = state.vocabulary.filter((item) => item.name !== action.payload);
-      },
-      markAchieved: (state, action) => {
+      });
+
+      builder.addCase(markAchieved.fulfilled, (state, action) => {
          state.vocabulary = state.vocabulary.map((elem, index) => {
             if (index === action.payload) {
                elem.status = elem.status === 'achieved' ? 'new' : 'achieved';
             }
             return elem;
          });
-      },
-      markPractice: (state, action) => {
+      });
+
+      builder.addCase(markPractice.fulfilled, (state, action) => {
          state.vocabulary = state.vocabulary.map((elem, index) => {
             if (index === action.payload) {
                elem.status = elem.status === 'practice' ? 'new' : 'practice';
             }
             return elem;
          });
-      },
-      sortByName: (state) => {
+      });
+
+      builder.addCase(sortByName.fulfilled, (state) => {
          state.vocabulary = [...state.vocabulary].sort((a, b) => (a.name < b.name ? -1 : 0));
-      },
-      sortByStatus: (state) => {
+      });
+
+      builder.addCase(sortByStatus.fulfilled, (state) => {
          state.vocabulary = [...state.vocabulary].sort((a, b) => (a.status < b.status ? -1 : 0));
-      },
-      sortRandom: (state) => {
+      });
+
+      builder.addCase(sortRandom.fulfilled, (state) => {
          state.vocabulary = shuffleAndCut([...state.vocabulary], state.vocabulary.length);
-      }
+      });
    }
 });
 
