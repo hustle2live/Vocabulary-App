e483f7a6a7f7ecc03b2a1e7b89b4c31a6757e27e
hustle2live <kuz.v@ukr.net>
2024-02-05 14:51:49 +0200

    add material design icons with bulms css

--- src/components/Vocabulary/Vocabulary.jsx
+++ src/components/Vocabulary/Vocabulary.jsx
@@ -53,27 +53,62 @@ export const Vocabulary = () => {
             <p className={styles['word-name']}>{name}</p>
             <p className={styles['word-translate']}>{translate}</p>
             <button
-               className={styles.deleteButton}
+               className={`${styles.deleteButton} delete is-medium`}
                label="delete word from list"
                onClick={() =>
                   window.confirm('do you realy wnt to delete word ?')
                      ? deleteWordHandler(name)
                      : null
                }
-            >
-               <span className="material-symbols-rounded">delete</span>
-            </button>
+            ></button>
          </li>
       ));
 
    return (
       <div className={styles.wrapper}>
+         <div className="">
+            <h1 className="title">Hello, </h1>
+
+            <p className="subtitle">
+               Wellcome to Modern React Vocabulary created by{' '}
+               <a href="https://github.com/hustle2live" target="_blank">
+                  Volodymyr K.
+               </a>
+            </p>
+
+            <div className="field">
+               <div className="control">
+                  <input className="input" type="text" placeholder="Input" />
+               </div>
+            </div>
+
+            <div className="field">
+               <p className="control">
+                  <span className="select">
+                     <select>
+                        <option>Select dropdown</option>
+                        <option>Select dropdown</option>
+                        <option>Select dropdown</option>
+                     </select>
+                  </span>
+               </p>
+            </div>
+
+            <div className="buttons">
+               <div href="" className="button is-primary">
+                  Primary
+               </div>
+               <div href="" className="button is-link">
+                  Link
+               </div>
+            </div>
+         </div>
          <div className={styles.content}>
             <ul className={styles.wordList}>
                <WordsUlListEleent />
             </ul>
-            <AddWordButton />
          </div>
+         <AddWordButton />
       </div>
    );
 };
