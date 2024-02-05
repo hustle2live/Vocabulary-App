6ac3ea9445ed921d1dfba2dc30c680f23b7a749b
hustle2live <kuz.v@ukr.net>
2024-02-05 15:42:55 +0200

    moderate instructions section

--- src/components/Vocabulary/Vocabulary.jsx
+++ src/components/Vocabulary/Vocabulary.jsx
@@ -42,72 +42,77 @@ export const Vocabulary = () => {
       [dispatch],
    );
 
-   const WordsUlListEleent = () =>
-      vocabulary.map(({ name, translate, status }, index) => (
-         <li
-            className={styles.wordListElement}
-            key={index}
-            onDoubleClick={() => markAsLearnedHandler(index)}
-         >
-            <RatingStatus status={status} />
-            <p className={styles['word-name']}>{name}</p>
-            <p className={styles['word-translate']}>{translate}</p>
-            <button
-               className={`${styles.deleteButton} delete is-medium`}
-               label="delete word from list"
-               onClick={() =>
-                  window.confirm('do you realy wnt to delete word ?')
-                     ? deleteWordHandler(name)
-                     : null
-               }
-            ></button>
-         </li>
-      ));
+   const WordsUlListElement = () => (
+      <div className={styles.content}>
+         <ul className={styles.wordList}>
+            {vocabulary.map(({ name, translate, status }, index) => (
+               <li
+                  className={styles.wordListElement}
+                  key={index}
+                  onDoubleClick={() => markAsLearnedHandler(index)}
+               >
+                  <RatingStatus status={status} />
+                  <p className={styles['word-name']}>{name}</p>
+                  <p className={styles['word-translate']}>{translate}</p>
+                  <button
+                     className={`${styles.deleteButton} delete is-medium`}
+                     label="delete word from list"
+                     onClick={() =>
+                        window.confirm('do you realy wnt to delete word ?')
+                           ? deleteWordHandler(name)
+                           : null
+                     }
+                  ></button>
+               </li>
+            ))}
+         </ul>
+      </div>
+   );
+
+   const Instructions = () => (
+      <div className={styles.instructions}>
+         <h1 className="title">Hello, </h1>
+
+         <p className="subtitle">
+            Wellcome to Modern React Vocabulary created by{' '}
+            <a href="https://github.com/hustle2live" target="_blank">
+               Volodymyr K.
+            </a>
+         </p>
+
+         <div className="field">
+            <div className="control">
+               <input className="input" type="text" placeholder="Input" />
+            </div>
+         </div>
+
+         <div className="field">
+            <p className="control">
+               <span className="select">
+                  <select>
+                     <option>Select dropdown</option>
+                     <option>Select dropdown</option>
+                     <option>Select dropdown</option>
+                  </select>
+               </span>
+            </p>
+         </div>
+
+         <div className="buttons">
+            <div href="" className="button is-primary">
+               Primary
+            </div>
+            <div href="" className="button is-link">
+               Link
+            </div>
+         </div>
+      </div>
+   );
 
    return (
       <div className={styles.wrapper}>
-         <div className="">
-            <h1 className="title">Hello, </h1>
-
-            <p className="subtitle">
-               Wellcome to Modern React Vocabulary created by{' '}
-               <a href="https://github.com/hustle2live" target="_blank">
-                  Volodymyr K.
-               </a>
-            </p>
-
-            <div className="field">
-               <div className="control">
-                  <input className="input" type="text" placeholder="Input" />
-               </div>
-            </div>
-
-            <div className="field">
-               <p className="control">
-                  <span className="select">
-                     <select>
-                        <option>Select dropdown</option>
-                        <option>Select dropdown</option>
-                        <option>Select dropdown</option>
-                     </select>
-                  </span>
-               </p>
-            </div>
-
-            <div className="buttons">
-               <div href="" className="button is-primary">
-                  Primary
-               </div>
-               <div href="" className="button is-link">
-                  Link
-               </div>
-            </div>
-         </div>
-         <div className={styles.content}>
-            <ul className={styles.wordList}>
-               <WordsUlListEleent />
-            </ul>
-         </div>
+         <Instructions />
+         <WordsUlListElement />
          <AddWordButton />
       </div>
    );
